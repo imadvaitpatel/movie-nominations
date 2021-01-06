0c8f1732f5807acde8fdc098c0dbea2d797c9c34
Advait Patel <advait.patel2010@gmail.com>
2021-01-05 19:56:34 -0500

    results now updates when user types

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import {SearchBox } from './components.js';
+import ParentContainer from './components.js';
 
 function App() {
   return (
     <div className="App">
-      <div className="flex-container">
-        <div className="header">
-          The Shoppies
-        </div>
-        <SearchBox/>
-        <div className="practice">
-          hi
-        </div>
-      </div>
+      <ParentContainer/>
     </div>
   );
 }
--- src/components.js
+++ src/components.js
@@ -1,26 +1,74 @@
 import React from 'react';
 import './components.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-export const SearchBox = () => {
-  return ( 
-    <div className="search-box">
-        <div className="movie-title">
-            Movie Title
-        </div>
-        <div className="search-bar">
-          <input type="text"/>
-          <FontAwesomeIcon icon={faSearch} className="search-icon" styles="color: blue;"/>
-        </div>
-    </div>
-  )
-}
+const SearchBox = (props) => {
+  const {callback} = props;
 
-export const ResultBox = () => {
-  return (
-    <div className="result-box">
-      
-    </div>
-  )
+  const onInputChange = (event) => {
+    callback(event.target.value);
+  }
+
+	return (
+		<div className='search-box'>
+			<div className='movie-title'>Movie Title</div>
+			<div className='search-bar'>
+				<input type='text' onInput={onInputChange}/>
+				<FontAwesomeIcon
+					icon={faSearch}
+					className='search-icon'
+					styles='color: blue;'
+				/>
+			</div>
+		</div>
+	);
+};
+
+const ResultBox = (props) => {
+  const {result} = props;
+  console.log(`new result :: ${result}`);
+  console.log(result);
+	return (
+		<div className='result-box'>
+			<div>Results for "{result}"</div>
+		</div>
+	);
+};
+
+const NominationBox = () => {
+	return (
+		<div className='nomination-box'>
+			<div>Nominations</div>
+		</div>
+	);
+};
+
+export default class ParentContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      nominations: [],
+      result: '',
+    };
+  }
+  
+  updateResults = (newResult) => {
+    console.log(newResult);
+    this.setState({result: newResult});
+    console.log('updating');
+  }
+
+	render() {
+		return (
+			<div className='flex-container'>
+				<div className='header'>The Shoppies</div>
+				<SearchBox callback={this.updateResults}/>
+				<div className='results-container'>
+					<ResultBox result={this.state.result}/>
+					<NominationBox />
+				</div>
+			</div>
+		);
+	}
 }
